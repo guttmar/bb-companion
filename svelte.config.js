a905095ffafd5fc1d2ed3309674accd002f887aa
Viktor Andersson <viktor.andersson@mindified.com>
2025-12-21 12:55:34 +0100

    ci: deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined
+		}),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/bb-companion' : ''
+		}
+	}
 };
 
 export default config;
